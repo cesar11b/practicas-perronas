300d27a135695657b3d49ad0a60aa984e1f9091c
cesar11b <katanavivas@gmail.com>
2021-10-11 11:28:32 -0600

    Agregar archivos de proyecto.

---
+++ enlace github/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace enlace_github
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnhola = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnhola
+            // 
+            this.btnhola.Location = new System.Drawing.Point(13, 13);
+            this.btnhola.Name = "btnhola";
+            this.btnhola.Size = new System.Drawing.Size(75, 23);
+            this.btnhola.TabIndex = 0;
+            this.btnhola.Text = "hola";
+            this.btnhola.UseVisualStyleBackColor = true;
+            this.btnhola.Click += new System.EventHandler(this.btnhola_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnhola);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnhola;
+    }
+}
+
