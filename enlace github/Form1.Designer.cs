865752a4c8fededd96d7197c7e9b9557441e2fd5
cesar11b <katanavivas@gmail.com>
2021-10-11 11:32:20 -0600

    when momo papuh k rika elfa :VVVVVVVV

--- enlace github/Form1.Designer.cs
+++ enlace github/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace enlace_github
         private void InitializeComponent()
         {
             this.btnhola = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnhola
@@ -42,22 +44,45 @@ namespace enlace_github
             this.btnhola.UseVisualStyleBackColor = true;
             this.btnhola.Click += new System.EventHandler(this.btnhola_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ":V perro";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnhola);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnhola;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
